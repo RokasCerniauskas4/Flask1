a87ef78f1d658a9a07de1595109d296d192ec092
Rokas Černiauskas <Rokascerniauskas4@gmail.com>
2022-11-16 19:50:28 +0200

    Sukeliamas darbas

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('homepage.html')
+
+@app.route('/clients')
+def show_clients():
+    clients = [
+        {'id': 1, 'name': 'CompanyName1'},
+        {'id': 2, 'name': 'CompanyName2'},
+        {'id': 3, 'name': 'CompanyName3'},
+        {'id': 4, 'name': 'CompanyName4'},
+        {'id': 5, 'name': 'CompanyName5'},
+    ]
+    return render_template('clients.html', data=clients)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
